8be0e8998252c6537be26d2bedcd384d9136379c
cetecom <cetecom@LC08L1314>
2016-04-27 20:23:28 -0300

    QUERY

--- db_constantes.php
+++ db_constantes.php
@@ -2,7 +2,7 @@
 /*Opciones de conexión a la base de datos*/
 
 define("MYSQL_SERVER","mysql:host=localhost");
-define("MYSQL_USER","root");
+define("MYSQL_USER","root@localhost");
 define("MYSQL_PASS","");
 define("MYSQL_DB","dbusuarios");
 
